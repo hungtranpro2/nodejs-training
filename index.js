63ce22c304471c6688f8c8bf47a7ca91937815fc
Hung Tran <hungtranpro2@gmail.com>
2020-07-21 17:41:03 +0700

    Building API and Working Database

---
+++ index.js
@@ -0,0 +1,14 @@
+// import express from "express";
+// import consign from "consign";
+const express = require("express");
+const consign = require("consign");
+
+const app = express();
+
+consign()
+  .include("./libs/config.js")
+  .then("db.js")
+  .then("libs/middlewares.js")
+  .then("routes")
+  .then("libs/boot.js")
+  .into(app);
