92760bdebc80138fdddf1f8095da4b693667f625
Hung Tran <hungtranpro2@gmail.com>
2020-07-27 09:56:15 +0700

    Documenting the API

--- routes/token.js
+++ routes/token.js
@@ -4,6 +4,23 @@ module.exports = (app) => {
   const cfg = app.libs.config;
   const Users = app.db.models.Users;
 
+  /**
+   * @api {post} /token Authentication Token
+   * @apiGroup Credentials
+   * @apiParam {string} email User email
+   * @apiParam {string} password User password
+   * @apiParamExample {json} Input
+   *    {
+   *      "email": "john@connor.net",
+   *      "password": "123456"
+   *    }
+   * @apiSuccess {String} token Token of authenticated user
+   * @apiSuccessExample {json} Success
+   *    HTTP/1.1 200 OK
+   *    {"token": "xyz.abc.123.hgf"}
+   * @apiErrorExample {json} Authentication error
+   *    HTTP/1.1 401 Unauthorized
+   */
   app.post("/token", (req, res) => {
     if (req.body.email && req.body.password) {
       const email = req.body.email;
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,25 @@ module.exports = (app) => {
   app
     .route("/user")
     .all(app.auth.authenticate())
+    /**
+     * @api {get} /user Return the authenticated user's data
+     * @apiGroup User
+     * @apiHeader {string} Authorization Token of authenticated user
+     * @apiHeaderExample {json} Header
+     *    {"Authorization": "JWT xyz.abc.123.hgf"}
+     * @apiSuccess {Number} id User id
+     * @apiSuccess {String} name User name
+     * @apiSuccess {String} email User email
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "id": 1,
+     *      "name": "john Connor",
+     *      "email": "john@connor.net"
+     *    }
+     * @apiErrorExample {json} Find error
+     *    HTTP/1.1 412 Precondition Failed
+     */
     .get((req, res) => {
       Users.findByPk(req.user.id, {
         attributes: ["id", "name", "email"],
@@ -13,6 +32,17 @@ module.exports = (app) => {
           res.status(412).json({ msg: error.message });
         });
     })
+    /**
+     * @api {delete} /user Deletes an authenticated user
+     * @apiGroup User
+     * @apiHeader {String} Authorization Token of authenticated user
+     * @apiHeaderExample {json} Header
+     *    {"Authorization": "JWT xyz.abc.123.hgf"}
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 204 No Content
+     * @apiErrorExample {json} Delete error
+     *    HTTP/1.1 412 Precondition Failed
+     */
     .delete((req, res) => {
       Users.destroy({ where: { id: req.user.id } })
         .then((result) => res.sendStatus(204))
@@ -20,7 +50,36 @@ module.exports = (app) => {
           res.status(412).json({ msg: error.message });
         });
     });
-
+  /**
+   * @api {post} /users Register a new user
+   * @apiGroup User
+   * @apiParam {string} name User name
+   * @apiParam {string} email User email
+   * @apiParam {string} password User Password
+   * @apiParamExample {json} Input
+   *    {
+   *      "name": "john Connor",
+   *      "email": "john@connor.net",
+   *      "password": "123456"
+   *    }
+   * @apiSuccess {Number} id User id
+   * @apiSuccess {String} name USer name
+   * @apiSuccess {String} email USer email
+   * @apiSuccess {String} password User encrypted password
+   * @apiSuccess {Date} update_at Update's date
+   * @apiSuccess {Date} create_at Register's date
+   * @apiSuccessExample {json} Success
+   *    {
+   *      "id": 1,
+   *      "name": "john Connor",
+   *      "email": "john@connor.net",
+   *      "password": "$2a$10$SK1B1",
+   *      "update_at": "2016-02-10T15:20:11.700Z",
+   *      "create_at": "2016-02-10T15:29:11.700Z",
+   *    }
+   * @apiErrorExample {json} Register error
+   *    HTTP/1.1 412 Precondition Failed
+   */
   app.post("/users", (req, res) => {
     Users.create(req.body)
       .then((result) => res.json(result))
